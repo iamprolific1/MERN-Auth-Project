760764151d0860bb9d49c2ccaf4f8b942c7b25fa
prolific <abdulmalikabdulrahman76@gmail.com>
2024-06-01 09:13:28 +0100

    database configuration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import connectDB from './config/db.js'
+
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
-app.listen(3000, ()=>{
+connectDB();
+app.listen(PORT, ()=>{
     console.log('Server is running on port 3000');
 })
